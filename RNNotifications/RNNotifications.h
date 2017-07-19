9a60a03719bbffffceb5645888969bdc7bbcaa87
Tolu Olowu <o.tolulope@gmail.com>
2017-07-19 20:39:16 +0100

    Add fetchCompletionHandler to didReceiveRemoteNotification

--- RNNotifications/RNNotifications.h
+++ RNNotifications/RNNotifications.h
@@ -14,8 +14,8 @@
 + (void)didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings;
 + (void)didUpdatePushCredentials:(PKPushCredentials *)credentials forType:(NSString *)type;
 
-+ (void)didReceiveRemoteNotification:(NSDictionary *)notification;
-+ (void)didReceiveLocalNotification:(UILocalNotification *)notification fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler;
++ (void)didReceiveRemoteNotification:(NSDictionary *)notification fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler;
++ (void)didReceiveLocalNotification:(UILocalNotification *)notification;
 
 + (void)handleActionWithIdentifier:(NSString *)identifier forRemoteNotification:(NSDictionary *)userInfo withResponseInfo:(NSDictionary *)responseInfo completionHandler:(void (^)())completionHandler;
 + (void)handleActionWithIdentifier:(NSString *)identifier forLocalNotification:(UILocalNotification *)notification withResponseInfo:(NSDictionary *)responseInfo completionHandler:(void (^)())completionHandler;
